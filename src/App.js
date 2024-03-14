c2d4439b1eefe0c537bc17656da4c2ea6b3978b6
Temiloluwaakin <106915700+Temiloluwaakin@users.noreply.github.com>
2024-03-14 22:47:28 +0100

    analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Route, Routes } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import Homepage from "./components/Homepage";
+import { Analytics } from '@vercel/analytics/react';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className="App">
+      <Analytics />
       <Routes>
           <Route path="/" element={ <Welcome /> } />
           <Route path='/homepage' element={ <Homepage/> } />
